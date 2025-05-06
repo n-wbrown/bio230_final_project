1280d6a1288d58d55f60520acedc0c6888bd05b0
n-wbrown <n-wbrown@users.noreply.github.com>
2025-05-05 18:51:52 -0700

    remove example file

--- src/diff_eq_generator.py
+++
@@ -1,9 +0,0 @@
-"""
-diff_eq_generator.py
-
-Contains the code used for generating the differential equations that describe
-a system.
-"""
-
-def example_function():
-    print(f"the example function in {__file__} is running")
